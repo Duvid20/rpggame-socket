d1afd52d73534322e7b70eab8a82d7be96d28946
Duvid20 <davidgabriel1988aug@gmail.com>
2023-12-25 13:41:58 +0100

    Commented out playerMoved event emission

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ io.on("connection", async (socket) => {
   // user moves
   socket.on("move", (position) => {
     console.log("Moving user id " + socket.id);
-    socket.to(roomName).emit("playerMoved", { id: socket.id, position });
+    //socket.to(roomName).emit("playerMoved", { id: socket.id, position });
   });
 });
 
